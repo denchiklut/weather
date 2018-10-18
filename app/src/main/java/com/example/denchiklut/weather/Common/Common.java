1b77de777010b1b2919cc79c7812d534b75f86d0
Denis <denchiklut@gmail.com>
2018-10-18 01:10:28 -0700

    part 1 done

--- app/src/main/java/com/example/denchiklut/weather/Common/Common.java
+++ app/src/main/java/com/example/denchiklut/weather/Common/Common.java
@@ -1,6 +1,9 @@
 package com.example.denchiklut.weather.Common;
 
+import android.location.Location;
+
 public class Common {
 
     public static final String APP_ID = "a3b66e3d37fce792efa7a0d9634876b1";
+    public static Location current_location = null;
 }
